e6cf3535ec0b6d6bde5ba6da1163bb0756c6c8ce
Vadym907 <vad90700@gmail.com>
2025-11-28 18:33:45 +0200

    Webstudio

---
+++ modal.js
@@ -0,0 +1,10 @@
+(() => {
+    const modal = document.getElementById("modal");
+    const openBtn = document.getElementById("openModal");
+    const closeBtn = document.querySelector(".solution--modal--content--close");
+
+    openBtn.onclick = () => modal.style.display = "flex";
+    closeBtn.onclick = () => modal.style.display = "none";
+    window.onclick = e => { if (e.target === modal) modal.style.display = "none"; };
+
+})();
