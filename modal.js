3bfba8a3378e9e28a8ad7238fc9c095026f20109
Vadym907 <vad90700@gmail.com>
2025-12-05 16:37:16 +0200

    Webstudio

--- modal.js
+++ modal.js
@@ -8,3 +8,10 @@
     window.onclick = e => { if (e.target === modal) modal.style.display = "none"; };
 
 })();
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+    menu.classList.toggle('active');
+});
